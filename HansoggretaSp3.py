66624d0edddf0448dca7fc5e2d23a683fe3f2b74
hansoggreta <36301640+hansoggreta@users.noreply.github.com>
2018-03-11 12:01:19 +0000

    Update HansoggretaSp3.py

--- HansoggretaSp3.py
+++ HansoggretaSp3.py
@@ -12,12 +12,15 @@ class Herbergi(HansogGretaSp3):
     def __init__(self, name, herb):
         self.name= name
         self.herb= herb
-        if self.herb == 1:
+        self.StartingProblem()
+        #if self.herb == 1:
             #self.herb='Herbergi 1'
-            self.StartingProblem()
-        elif self.herb == 2:
+            #self.StartingProblem()
+        #elif self.herb == 2:
             #self.herb='Herbergi 2'
-            self.StartingProblem()
+            #self.StartingProblem()
+        #elif self.herb == 3:
+            #self.StartingProblem()
 
     def StartingProblem(self): #Method sem býr til þraut, g.r.f. að hluturinn self.herb haldist hér úr smið, t.d. ef self.herb == 1 þá helst það hér
         description= self.getDescription()
@@ -26,6 +29,8 @@ class Herbergi(HansogGretaSp3):
             self.problem1()
         elif self.herb == 2:
             self.problem2()
+        elif self.herb == 3:
+            self.problem3()
 
     def problem1(self):
         print('Hvað er 1+1?')
@@ -35,21 +40,44 @@ class Herbergi(HansogGretaSp3):
             print('Rangt svar. Reyndu aftur!')
             print('Hvað er 1+1?')
             s1 = input('Mitt svar:')
+            s1 = int(s1)
         print('Rétt hjá þér! Áfram í næsta herbergi.')
         return
 
-    def problem2(self):pass
+    def problem2(self):
+        print('Hvað er 2+2?')
+        s1 = input('Mitt svar:')
+        s1 = int(s1)
+        while(s1 != 4):
+            print('Rangt svar. Reyndu aftur!')
+            print('Hvað er 1+1?')
+            s1 = input('Mitt svar:')
+            s1 = int(s1)
+        print('Rétt hjá þér! Áfram í næsta herbergi.')
+        return
+
+    def problem3(self):
+        print('Hvað er 3+3?')
+        s1 = input('Mitt svar:')
+        s1 = int(s1)
+        while(s1 != 6):
+            print('Rangt svar. Reyndu aftur!')
+            print('Hvað er 1+1?')
+            s1 = input('Mitt svar:')
+            s1 = int(s1)
+        print('Rétt hjá þér! Áfram í næsta herbergi.')
+        return
 
     def getName(self):
         return self.name
     def getDescription(self):
+        print('Þú ert í herberginu:', self.name)
         if self.herb == 1:
-            print('Þú ert í herberginu:', self.name)
             return 'Herbergið þar sem vonda nornin horfir á sjónvarpið'
-        else:
+        elif self.herb == 2:
             return 'Herbergið þar sem vonda nornin sefur'
-        #else:
-        #    return 'Herbergið þar sem vonda nornin fer á klósettið'
+        elif self.herb == 3:
+            return 'Herbergið þar sem vonda nornin tannburstar sig'
 
 class Persona(Herbergi):
     def __init__(self, kyn, name, description):
@@ -97,7 +125,10 @@ def main():
         print('Þú ert komin/n í svefnherbergið. Gangi þér vel að leysa þrautina.')
         herb=2
         h2=Herbergi('Svefnherbergi', herb)
-        break
-
+        print('Þú ert komin/n í baðherbergið. Gangi þér vel að leysa þrautina.')
+        herb=3
+        h3=Herbergi('Baðherbergi', herb)
+        win == True
+    print('Þú hefur unnið leikinn!')
 if __name__ == "__main__":
     main()
